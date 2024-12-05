ed1c94b2f7ddba6cc32a18d17a97ff5f8f407195
MauroRaya <maurorayabr@gmail.com>
2024-12-05 10:05:29 -0300

    feat: add getById route

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"errors"
 	"net/http"
 )
 
@@ -18,14 +19,39 @@ var pets = []pet {
 	{Id: "3", Name: "Jake",   Species: "Cat",    DateOfBirth: "12/01/2019"},
 }
 
+func getPetById(id string) (*pet, error) {
+	for i := range pets {
+		if pets[i].Id == id {
+			return &pets[i], nil
+		}
+	}
+	return nil, errors.New("pet not found")
+}
+
 func getPets(context *gin.Context) {
 	context.JSON(http.StatusOK, pets)
+}
+
+func getPet(context *gin.Context) {
+	id := context.Param("id")
+
+	pet, err := getPetById(id)
+	if err != nil {
+		context.JSON(http.StatusNotFound, gin.H{
+			"success": false,
+			"error": err.Error(),
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, pet)
 }
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/pets", getPets)	
+	router.GET("/pets",     getPets)
+	router.GET("/pets/:id", getPet)	
 
 	router.Run("localhost:80")
 }
