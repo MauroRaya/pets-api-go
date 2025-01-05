607b5655969d6d1f43b51feba40cf22e5c8d0999
MauroRaya <maurorayabr@gmail.com>
2025-01-04 22:21:52 -0300

    fix: add acr to main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,42 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "pets_rg" {
-  name     = "pets_rg"
-  location = "East US"
+  name     = "pets-rg"
+  location = "Canada Central"
+}
+
+resource "azurerm_container_registry" "pets_acr" {
+  name                = "petsacr987"
+  location            = azurerm_resource_group.pets_rg.location
+  resource_group_name = azurerm_resource_group.pets_rg.name
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_app_service_plan" "pets_plan" {
+  name                = "pets-service-plan"
+  location            = azurerm_resource_group.pets_rg.location
+  resource_group_name = azurerm_resource_group.pets_rg.name
+  kind                = "Linux"
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "pets_api" {
+  name                = "pets-api"
+  location            = azurerm_resource_group.pets_rg.location
+  resource_group_name = azurerm_resource_group.pets_rg.name
+  app_service_plan_id = azurerm_app_service_plan.pets_plan.id
+
+  site_config {
+    linux_fx_version = "DOCKER|pets_image:latest"
+  }
+
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+    DOCKER_ENABLE_CI                    = "true"
+  }
 }
